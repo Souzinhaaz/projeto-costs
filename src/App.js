751ae5c3be13b5f2931cbf10163c9e71e174d97d
Souzinhaaz <gustavodesouzaoliveira1432@gmail.com>
2024-06-06 04:44:57 -0300

    aula 18

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+        <Container customClass="min-height">
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/contact' element={<Contact />} />
+              <Route path='/company' element={<Company />} />
+              <Route path='/newproject' element={<NewProject />} />
+          </Routes>
+        </Container>
+
+      <p>Footer</p>
+    </Router>
   );
 }
 
