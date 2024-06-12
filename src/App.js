09304a37ee6f80766ffa047df50f3c9290f0f841
Souzinhaaz <gustavodesouzaoliveira1432@gmail.com>
2024-06-12 18:48:32 -0300

    aula 21

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
+        <Navbar />
         <Container customClass="min-height">
           <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/contact' element={<Contact />} />
+              <Route path='/projects' element={<Projects />} />
               <Route path='/company' element={<Company />} />
               <Route path='/newproject' element={<NewProject />} />
           </Routes>
         </Container>
 
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
